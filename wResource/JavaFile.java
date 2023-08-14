bf831e7612ef9840e89d721afac014cdd501113f
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-08-15 01:54:12 +0530

    Delete JavaFile.java

--- wResource/JavaFile.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class JavaFile{
-	public static void main(String[] args){
-		// System.out.println("Hello World..! My Name is Prerak..!");
-		Scanner sc = new Scanner(System.in);
-		System.out.println(Math.pow(3, 9));	
-	}
-}
