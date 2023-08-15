7b04a4b748782cbf9c58982ff91b8ecfd567c164
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-08-15 18:55:52 +0530

    Update Print_Patterns.java

--- wResource/Print_Patterns.java
+++ wResource/Print_Patterns.java
@@ -13,7 +13,21 @@ public class Print_Patterns{
 		// printRowNumberTriangle(n);
 		// printPyramid(n);
 		// printFloydTriangle(n);
-		printDaimond(n);
+		// printDaimond(n);
+		printPascalsTriangle(n);
+	}
+	public static void printPascalsTriangle(int n){
+		int val = 0;
+		for(int i = 1; i <= n; i++){
+			for(int j = 1; j <= n - i; j++){
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++){
+				if(i == 1 || j == 1){ val = 1; } 
+				else{ val = val * (i - j + 1) / (j - 1); } 
+				System.out.print(val + " ");
+			} System.out.println();
+		}
 	}
 	public static void printDaimond(int n){
 		int nst = 1, nsp = n - 1;
