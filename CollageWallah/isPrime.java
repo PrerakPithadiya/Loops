e48451d1145d288d07dcd72809195616d5f6fe56
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-06 14:35:14 +0530

    Update isPrime.java

--- CollageWallah/isPrime.java
+++ CollageWallah/isPrime.java
@@ -17,8 +17,17 @@ public class isPrime{
 		System.out.println("Result is : The given Number " + result + " a Prime!");
 	}
 	public static boolean isPrime(int n){
-		for(int i = 2; i < n; i++){
-			if(n % i == 0){
+		if(n == 1){
+			return false;
+		}
+		if(n == 2 || n == 3){
+			return true;
+		}
+		if(n % 2 == 0 || n % 3 == 0){
+			return false;
+		}
+		for(int i = 5; i * i <= n; i = i + 6){
+			if(n % i == 0 || n % (i + 2) == 0){
 				return false;
 			}
 		}
