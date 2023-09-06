a5be916c5561d8cfceb829a4a7c2c57e7135b6c7
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-06 14:38:23 +0530

    Update Is_Prime_Number.java

--- wResource/Is_Prime_Number.java
+++ wResource/Is_Prime_Number.java
@@ -12,8 +12,17 @@ public class Is_Prime_Number{
 		}
 	}
 	public static boolean isPrime(int n){
-		for(int i = 2; i <= n - 1; i++){
-			if(n % i == 0){
+		if(n == 1){
+			return false;
+		}
+		if(n == 2 || n == 3){
+			return true;
+		}
+		if(n % 2 == 0 || n % 3 == 0){
+			return false;
+		}
+		for(int i = 5; i * i <= n; i = i + 6){
+			if(n % i == 0 || n % (i + 2) == 0){
 				return false;
 			}
 		}
