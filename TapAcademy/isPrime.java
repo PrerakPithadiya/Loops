81d8e788ca838b7ec4b0736b2e2ff51312067311
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-06 14:33:17 +0530

    Add files via upload

---
+++ TapAcademy/isPrime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class isPrime{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number : ");
+		int n = sc.nextInt();
+		if(checkPrime(n)){
+			System.out.println("The given Number '" + n + "' is Prime!");
+		} else{
+			System.out.println("The given Number '" + n + "' isn't Prime!");
+		}
+	}
+	public static boolean checkPrime(int n){
+		if(n == 1){
+			return false;
+		}
+		if(n == 2 || n == 3){
+			return true;
+		}
+		if(n % 2 == 0 || n % 3 == 0){
+			return false;
+		}
+		for(int i = 5; i * i <= n; i = i + 6){
+			if(n % i == 0 || n % (i + 2) == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+}
